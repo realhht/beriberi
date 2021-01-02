5e292f938ee436ba5e9e10693771b62711d7b964
SometimesNaive <xjtuhht@163.com>
2021-01-02 21:07:38 +0800

    add sth

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Spring dfdf!!";
+        return "Hello Spring dfdffdf!!";
     }
 }
