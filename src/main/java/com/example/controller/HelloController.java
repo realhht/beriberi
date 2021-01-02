d64341f24a0ebddd633f6b197c7be727dd9298e4
SometimesNaive <xjtuhht@163.com>
2021-01-02 21:18:05 +0800

    test

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Spring dfdffdfdfdf!!";
+        return "Hello Spring dfdffdfdfdfdfdf!!";
     }
 }
